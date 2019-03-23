198e2cd4b8f6d7545e9597967868f04bae38a54b
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-03-22 20:46:06 -0600

    This is becoming a bit... complicated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,49 +4,63 @@ var userChoice;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var optA, optB, optC, optD, rAnswer;
-var time1 = 21;
+var optA, optB, optC, optD, rAnswer, explan;
+var time1 = 16;
 var intervalTime1;
 
 var questions = [
-    ["Question 1", "Option 1", "Option 2", "Option 3", "Option 4", "C"],
-    ["Question 2", "Option 21", "Option 22", "Option 23", "Option 24", "D"],
-    ["Question 3", "Option 31", "Option 32", "Option 33", "Option 34", "A"],
-    ["Question 4", "Option 41", "Option 42", "Option 43", "Option 44", "C"],
-    ["Question 5", "Option 51", "Option 52", "Option 53", "Option 54", "D"],
-    ["Question 6", "Option 61", "Option 62", "Option 63", "Option 64", "A"],
-    ["Question 7", "Option 71", "Option 72", "Option 73", "Option 74", "B"],
-    ["Question 8", "Option 81", "Option 82", "Option 83", "Option 84", "C"],
-    ["Question 9", "Option 91", "Option 92", "Option 93", "Option 94", "A"],
-    ["Question 10", "Option 101", "Option 102", "Option 103", "Option 104", "B"]
+    ["Question 1", "Nayru is the Goddess  of...","Power", "Wisdom", "Courage","Creation", "B"],
+    ["Question 2", "For which reason Yeto goes to Zora's Domain?", "Searching for a girlfriend", "To take a shower", "To search for Reekfish", "To search for a warmer place", "C"],
+    ["Question 3", "Ghirahim is the sword and servant of...", "Demise", "Ganondorf", "Batreaux", "Impa", "A"],
+    ["Question 4", "Where is the Thunder Dragon Lanayru located?", "Sandship", "Lanayru Mining Facility", "Silent Realm", "Lanayru Gorge", "D"],
+    ["Question 5", "With the infused power of the Four Elements the White Sword transforms into...", "The Four Sword", "The Master Sword", "Phantom Sword", "Biggoron's Sword", "A"],
+    ["Question 6", "Which one is the song Malon teaches Link?", "Song of Storms", "Epona's Song", "Scarecrow's Song", "Sun's Song", "B"],
+    ["Question 7", "Magical conductor's baton given to Link by the King of the Red Lions...", "Skull Kid's Horn", "Horse Grass", "Howling Stone", "Wind Waker", "D"],
+    ["Question 8", "Which one is the wish Linebeck asks to the Ocean King?", "To be rich", "To travel to another dimension", "To get back his ship", "To be immortal", "C"],
+    ["Question 9", "What is the name of the old man that guides Link in  A Link to the Past?", " Sahasrahla", "Gaepora", "Osfala", "Aginah", "A"],
+    ["Question 10", "Name of the tribe that have served the Royal Family of Hyrule since the age of creation", "Zora Tribe", "Hylian Tribe", "Sheikah Tribe", "Ninja Tribe", "C"]
 ];
 
+var explanations = ["Nayru is the Goddess of Wisdom, a recurring character in The Legend of Zelda franchise.",
+                    "In The Legend of Zelda: Twilight Princess, Yeto traveled from Snowpeak to Zora's Domain searching for Reekfish in order to make a soup to cure his wife, Yeta.",
+                    "Ghirahim is one of the main antagonists in The Legend of Zelda: Skyward Sword. He is the sword and servant of Demise. ",
+                    "The Lanayru Gorge is a location from The Legend of Zelda: Skyward Sword. Located to the south of Lanayru Desert, it is the dwelling place of Lanayru.",
+                    "The White Sword is the reforged Picori Blade. As soon as the power of the Four Elements is infused into the sword, it  becomes the sacred Four Sword, the strongest sword in The Legend of Zelda: The Minish Cap.",
+                    "Malon teaches Link 'Epona's Song' after he finds her father and makes him flee back to Lon Lon Ranch in The Legend of Zelda: Ocarina of Time.",
+                    "The Wind Waker is an item from The Legend of Zelda: The Wind Waker. It is a conductor's baton that Link uses to invoke several powers of nature by conducting disembodied voices.",
+                    "In The Legend of Zelda: Phantom Hourglass when Oshus asked for Linebeck's wish he states his one wish is to fix his boat which was destroyed in the final battle. ",
+                    "Throughout his adventure, Link can find Telepathic Tiles on the walls of dungeons that allows Sahasrahla to telepathically contact Link and give him advice.",
+                    "The Sheikah are a recurring race in the Legend of Zelda series. The Sheikah are an ancient clan of ninja-like warriors sworn to protect the Royal Family of Hyrule, even after death."];
 
 function displayQuestion() {
+    $(".container").show();
+    $(".startGame").hide();
     if (pos >= questions.length) {
-        $(".questions").html("<h2>Your Results: <h2>")
-        $(".answers").html("<h3> Correct answers: " + correct + "</h3><br>")
-        $(".answers").append("<h3>Wrong answers: " + incorrect + "</h3><br>");
-        $(".answers").append("<h3>Unanswered questions: " + unanswered + "</h3><br>");
+        showResults();
 
     } else {
         question = questions[pos][0];
-        optA = questions[pos][1];
-        optB = questions[pos][2];
-        optC = questions[pos][3];
-        optD = questions[pos][4];
-        rAnswer = questions[pos][5];
+        questionText = questions[pos][1];
+        optA = questions[pos][2];
+        optB = questions[pos][3];
+        optC = questions[pos][4];
+        optD = questions[pos][5];
+        rAnswer = questions[pos][6];
+        explan = explanations[pos];
         //Display questions
         $(".questions").html("<h3>" + question + "</h3>");
-        $(".answers").html("<div class='choices choice1 ' value='A'>" + optA + "</div><br>");
+        $(".questions").append("<h4>" + questionText + "</h4>");
+        $(".answers").html("<div class='choices choice1 ' value='A'> a) " + optA + "</div><br>");
         $(".choice1").val("A");
-        $(".answers").append("<div class='choices choice2 'value='B'>" + optB + "</div><br>");
+        $(".answers").append("<div class='choices choice2  'value='B'> b) " + optB + "</div><br>");
         $(".choice2").val("B");
-        $(".answers").append("<div class='choices choice3 'value='C'>" + optC + "</div><br>");
+        $(".answers").append("<div class='choices choice3  'value='C'> c) " + optC + "</div><br>");
         $(".choice3").val("C");
-        $(".answers").append("<div class='choices choice4 'value='D'>" + optD + "</div><br>");
+        $(".answers").append("<div class='choices choice4  'value='D'> d) " + optD + "</div><br>");
         $(".choice4").val("D");
         $(".answersContainer").hide();
+        $(".time-left").show();
+        $(".time-leftText").show();
         timetoAnswer();
         isRightAnswer();
 
@@ -63,33 +77,21 @@ function isRightAnswer() {
         if (userChoice == rAnswer) {
             $('.choices').off('click');
             rightAns();
-            /*correct++;
-            $(".answersContainer").show();
-            alert("Congratulations! The right answer is " + rAnswer);
-            console.log(correct);*/
         } else {
             $('.choices').off('click');
             wrongAns();
-            /*incorrect++;
-            $(".answersContainer").show();
-            alert("Wrong! The right answer is " + rAnswer);*/
-            console.log(incorrect);
         }
         resetTime();
         pos++;
-        setTimeout(displayQuestion, 5000);
-        //displayQuestion();
-        console.log(rAnswer);
-        console.log(pos + $(".choice4").val());
+        setTimeout(displayQuestion, 6000);
     });
 };
 
 
-
+ //Main
 $(document).ready(function () {
-
-    displayQuestion();
-    console.log(pos + rAnswer);
+    $(".container").hide();
+    startGame();
 });
 
 
@@ -100,14 +102,14 @@ function timetoAnswer() {
 
 function countDown() {
     time1--;
-    $(".time-left").html("<h4>" + time1 + "</h4>");
+    $(".time-left").html("<h1>" + time1 + "</h1>");
     if (time1 === 0) {
         $('.choices').off('click');
         stop();
         unAns();
         resetTime();
         pos++;
-        setTimeout(displayQuestion, 5000);
+        setTimeout(displayQuestion, 6000);
     }
 }
 
@@ -117,27 +119,55 @@ function stop() {
 }
 
 function resetTime() {
-    time1 = 21;
+    time1 = 16;
 }
 
 
 function rightAns() {
     correct++;
+    hideTimer();
     $(".answersContainer").show();
     $(".isAnswer").html("<h3>Congratulations!!</h3>");
-    $(".correctAnswer").html("<br><h4> The right answer is:<br> " + rAnswer+ "</h4>");
+    $(".correctAnswer").html("<br><h4> The right answer is " + rAnswer + "</h4><br>");
+    $(".correctAnswer").append("<p>" + explan + "</p>");
 };
 
 function wrongAns() {
     incorrect++;
+    hideTimer();
     $(".answersContainer").show();
     $(".isAnswer").html("<h3>Oops!!</h3>");
-    $(".correctAnswer").html("<br><p> The right answer is:<br> " + rAnswer);
+    $(".correctAnswer").html("<br><p> The right answer is " + rAnswer +"</h4><br>");
+    $(".correctAnswer").append("<p>" + explan + "</p>");
 };
 
-function unAns(){
+function unAns() {
     unanswered++;
+    hideTimer();
     $(".answersContainer").show();
     $(".isAnswer").html("<h3>Time's Up!!</h3>");
-    $(".correctAnswer").html("<br><p> The right answer is:<br> " + rAnswer);
+    $(".correctAnswer").html("<br><h4> The right answer is " + rAnswer + "</h4><br>");
+    $(".correctAnswer").append("<p>" + explan + "</p>");
 }
+
+function showResults() {
+    hideTimer();
+    $(".answersContainer").hide();
+    $(".questions").html("<h2>Your Results: <h2>")
+    $(".answers").html("<h3> Correct answers: " + correct + "</h3><br>")
+    $(".answers").append("<h3>Wrong answers: " + incorrect + "</h3><br>");
+    if (unanswered >= 1) {
+        $(".answers").append("<h3>Unanswered questions: " + unanswered + "</h3><br>");
+    }
+
+
+}
+
+function startGame(){
+    $(".startGame").click(displayQuestion);
+}
+
+function hideTimer(){
+    $(".time-left").hide();
+    $(".time-leftText").hide();
+};
